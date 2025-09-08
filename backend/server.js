c787a933711edb485fe6d1d2035f5bed32438b19
Krishna Agarwal <krishna016agrawal@gmail.com>
2025-09-08 16:29:58 +0530

    first commit

--- backend/server.js
+++ backend/server.js
@@ -6,10 +6,15 @@ import dotenv from "dotenv";
 dotenv.config();
 const app = express();
 app.use(bodyParser.json());
-app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:3000', 'http://127.0.0.1:5173',"https://readme-generator-vhzg.vercel.app"],
-  credentials: true
-}));
+// app.options("*", cors());
+
+// app.use(cors({
+//   origin: ['http://localhost:5173', 'http://localhost:3000', 'http://127.0.0.1:5173',"https://readme-generator-vhzg.vercel.app"],
+//   credentials: true
+// }));
+app.use(cors({ origin: true, credentials: true }));
+app.options("*", cors({ origin: true, credentials: true }));
+
 
 const PORT = 5000;
 const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
