fb980d475a23dbe2c5f51dc7cabfac1f59d932b1
Krishna Agarwal <krishna016agrawal@gmail.com>
2025-09-08 14:28:57 +0530

    first commit

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ dotenv.config();
 const app = express();
 app.use(bodyParser.json());
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:3000', 'http://127.0.0.1:5173',"https://readme-generator-vhzg-634061tgx-krishna-agarwal-s-projects.vercel.app"],
+  origin: ['http://localhost:5173', 'http://localhost:3000', 'http://127.0.0.1:5173',"https://readme-generator-vhzg.vercel.app"],
   credentials: true
 }));
 
